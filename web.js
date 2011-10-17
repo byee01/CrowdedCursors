43cb843a97c4efc793564f71063873b15fe7fb81
Brian Yee <brian.a.yee@gmail.com>
2011-10-16 22:01:36 -0400

    Adding basic styles, setting up canvas

--- web.js
+++ web.js
@@ -2,25 +2,23 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs');
 
-app.listen(500);
+app.listen(5000);
 
 function handler(request, response) {
   fs.readFile(__dirname + '/index.html',
   function (error, data) {
     if(error) {
-      res.writeHead(500);
-      return res.end("Error loading index.html");
+      response.writeHead(500);
+      return response.end("Error loading index.html");
     }
-
-    res.writeHead(200);
-    res.end(data);
+    response.writeHead(200);
+    response.end(data);
   });
 }
 
 
 io.sockets.on('connection', function(socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function(data) {
-    console.log(data);
-  });
+  socket.emit('status', { message: 'You have connected' });
+
+
 });
